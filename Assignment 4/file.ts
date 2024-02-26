cc1062ea61e07bb6177aff7505a3fb26496cd823
Tayyaba Farid <tayyabafarid7@gmail.com>
2024-02-26 22:21:34 +0500

    Added Assignment 4

---
+++ Assignment 4/file.ts
@@ -0,0 +1,4 @@
+// Assignment 4
+let message = "The greatest glory in living is not in falling, but in rising everytime we fall."
+let famous_person = "Nelson Mandela"
+console.log(`${famous_person} ` + "once said, " + `"${message}"`)
