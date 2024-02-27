ac52aa1d2382daef7375d316c535fe678ba8dd8f
Tayyaba Farid <tayyabafarid7@gmail.com>
2024-02-27 10:27:38 +0500

    updated Assignnment 6

--- Assignment 6/file.js
+++ Assignment 6/file.js
@@ -1,13 +1,4 @@
-// Assignment 6
-var num1 = 4;
-var num2 = 4;
-console.log("The addition of both numbers are ", num1 + num2);
-var num3 = 10;
-var num4 = 2;
-console.log("The subtraction of both numbers are ", num3 - num4);
-var num5 = 4;
-var num6 = 2;
-console.log("The multiplication of both numbers are ", num5 * num6);
-var num7 = 16;
-var num8 = 2;
-console.log("The division of both numbers are ", num7 / num8);
+var Fullname = "\t\n    Tayyaba Fareed    \t\n";
+console.log(Fullname);
+var newName = Fullname.trim();
+console.log(newName);
--- Assignment 6/file.ts
+++ Assignment 6/file.ts
@@ -1,16 +1,6 @@
-// Assignment 6
-let num1 = 4;
-let num2 = 4;
-console.log("The addition of both numbers are ", num1 + num2)
+// Assignment 5
+let Fullname = "\t\n    Tayyaba Fareed    \t\n"
+console.log(Fullname)
 
-let num3 = 10;
-let num4 = 2;
-console.log("The subtraction of both numbers are ", num3 - num4)
-
-let num5 = 4;
-let num6 = 2;
-console.log("The multiplication of both numbers are ", num5 * num6)
-
-let num7 = 16;
-let num8 = 2;
-console.log("The division of both numbers are ", num7 / num8)
+let newName = Fullname.trim()
+console.log(newName) 
