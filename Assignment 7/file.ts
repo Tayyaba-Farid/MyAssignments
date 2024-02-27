d9e4f0b4f0822b9988e2fb3102b4f2e5339a3c40
Tayyaba Farid <tayyabafarid7@gmail.com>
2024-02-27 10:28:17 +0500

    updated Assignment 7

--- Assignment 7/file.ts
+++ Assignment 7/file.ts
@@ -1,5 +1,16 @@
-// Assignment 7
-console.log(6 + 2)
-console.log(10-2)
-console.log(4*2)
-console.log(16/2)
+// Assignment 6
+let num1 = 4;
+let num2 = 4;
+console.log("The addition of both numbers are ", num1 + num2)
+
+let num3 = 10;
+let num4 = 2;
+console.log("The subtraction of both numbers are ", num3 - num4)
+
+let num5 = 4;
+let num6 = 2;
+console.log("The multiplication of both numbers are ", num5 * num6)
+
+let num7 = 16;
+let num8 = 2;
+console.log("The division of both numbers are ", num7 / num8)
