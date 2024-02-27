a871e37c38f5f28bb09e6e49e58157792b7556a5
Tayyaba Farid <tayyabafarid7@gmail.com>
2024-02-27 10:29:41 +0500

    added Assignment 10

---
+++ Assignment 10/file.js
@@ -0,0 +1,24 @@
+// Program 1
+var myName = "Tayyaba";
+var date = 27 / 2 / 2024;
+// comment: We stored numbers in the variables, then added, subtracted, multiplied and divided them individually by console.log
+var num1 = 4;
+var num2 = 4;
+console.log("The addition of both numbers are ", num1 + num2);
+var num3 = 10;
+var num4 = 2;
+console.log("The subtraction of both numbers are ", num3 - num4);
+var num5 = 4;
+var num6 = 2;
+console.log("The multiplication of both numbers are ", num5 * num6);
+var num7 = 16;
+var num8 = 2;
+console.log("The division of both numbers are ", num7 / num8);
+// program 2
+var userName = "Tayyaba";
+var fullDate = 27 / 2 / 2024;
+// comment: we simply run this program by adding, subtracting, multiplying and dividing numbers directly in the console.log. We did not declare variables this time. Just directly run program through console.log.
+console.log(6 + 2);
+console.log(10 - 2);
+console.log(4 * 2);
+console.log(16 / 2);
---
+++ Assignment 10/file.ts
@@ -0,0 +1,30 @@
+// Program 1
+let myName: string = "Tayyaba";
+let date: number = 27/2/2024
+
+// comment: We stored numbers in the variables, then added, subtracted, multiplied and divided them individually by console.log
+let num1 = 4;
+let num2 = 4;
+console.log("The addition of both numbers are ", num1 + num2)
+
+let num3 = 10;
+let num4 = 2;
+console.log("The subtraction of both numbers are ", num3 - num4)
+
+let num5 = 4;
+let num6 = 2;
+console.log("The multiplication of both numbers are ", num5 * num6)
+
+let num7 = 16;
+let num8 = 2;
+console.log("The division of both numbers are ", num7 / num8)
+
+// program 2
+let userName : string = "Tayyaba"
+let fullDate : number = 27/2/2024
+// comment: we simply run this program by adding, subtracting, multiplying and dividing numbers directly in the console.log. We did not declare variables this time. Just directly run program through console.log.
+
+console.log(6 + 2)
+console.log(10-2)
+console.log(4*2)
+console.log(16/2)
