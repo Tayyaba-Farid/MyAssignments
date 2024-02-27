135a918cb0fcd7e4ebd4c0c1930bfd1a277a0737
Tayyaba Farid <tayyabafarid7@gmail.com>
2024-02-27 10:14:28 +0500

    added Assignment 1

--- Assignment 1/file.js
+++
@@ -1,3 +0,0 @@
-// Assignment 1
-var fullname = "Eric";
-console.log("Hello Eric, would you like to learn some Python today?");
--- Assignment 1/file.ts
+++
@@ -1,4 +0,0 @@
-// Assignment 1
-
-let fullname = "Eric";
-console.log("Hello Eric, would you like to learn some Python today?")
