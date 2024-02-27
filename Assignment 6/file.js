3382e5d3e56380fea8d201a394e8fec0f193ebe9
Tayyaba Farid <tayyabafarid7@gmail.com>
2024-02-27 09:24:31 +0500

    Added assignment 6

---
+++ Assignment 6/file.js
@@ -0,0 +1,13 @@
+// Assignment 6
+var num1 = 4;
+var num2 = 4;
+console.log("The addition of both numbers are ", num1 + num2);
+var num3 = 10;
+var num4 = 2;
+console.log("The subtraction of both numbers are ", num3 - num4);
+var num5 = 4;
+var num6 = 2;
+console.log("The multiplication of both numbers are ", num5 * num6);
+var num7 = 16;
+var num8 = 2;
+console.log("The division of both numbers are ", num7 / num8);
