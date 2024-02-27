335390b0347e297a5ac9b12f438d614e57dfb615
Tayyaba Farid <tayyabafarid7@gmail.com>
2024-02-27 10:26:03 +0500

    updated Assignment 4

--- Assignment 4/file.js
+++ Assignment 4/file.js
@@ -1,4 +1,1 @@
-// Assignment 3
-var message = "The greatest glory in living is not in falling, but in rising everytime we fall.";
-var famous_person = "Nelson Mandela";
-console.log("".concat(famous_person, " ") + "once said, " + "\"".concat(message, "\""));
+console.log("Nelson Mandela once said, " + "\"The greatest glory in living is not in falling, but in rising everytime we fall.\"");
--- Assignment 4/file.ts
+++ Assignment 4/file.ts
@@ -1,4 +1,1 @@
-// Assignment 4
-let message = "The greatest glory in living is not in falling, but in rising everytime we fall."
-let famous_person = "Nelson Mandela"
-console.log(`${famous_person} ` + "once said, " + `"${message}"`)
+console.log("Nelson Mandela once said, " + `"The greatest glory in living is not in falling, but in rising everytime we fall."`)
