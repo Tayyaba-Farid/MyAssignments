dd3cbbc34f448c90e33d79f75bdd50d4ec29c2c4
Tayyaba Farid <tayyabafarid7@gmail.com>
2024-02-29 00:34:07 +0500

    Added Assignment 12

---
+++ Assignment 12/file.js
@@ -0,0 +1,4 @@
+var myFriends = ["Tayyaba", "Hania", "Iqra", "Aqsa", "Umaima"];
+for (var i = 0; i < myFriends.length; i++) {
+    console.log("Hi, " + "".concat(myFriends[i]) + "! " + "So nice to meet you!");
+}
---
+++ Assignment 12/file.ts
@@ -0,0 +1,5 @@
+let myFriends = ["Tayyaba", "Hania", "Iqra", "Aqsa", "Umaima"]
+for (let i = 0; i < myFriends.length; i++){
+    console.log("Hi, " + `${myFriends[i]}` + "! " + "So nice to meet you!")
+}
+
