61bf713f2e979ebd6170e92962d8b0eecbaba469
Tayyaba Farid <tayyabafarid7@gmail.com>
2024-02-29 00:32:58 +0500

    Added Assignment 11

---
+++ Assignment 11/file.js
@@ -0,0 +1,6 @@
+var array = ["Tayyaba", "Hania", "Iqra", "Aqsa", "Umaima"];
+console.log(array[0]);
+console.log(array[1]);
+console.log(array[2]);
+console.log(array[3]);
+console.log(array[4]);
---
+++ Assignment 11/file.ts
@@ -0,0 +1,7 @@
+let array = ["Tayyaba", "Hania", "Iqra", "Aqsa", "Umaima"]
+console.log(array[0])
+console.log(array[1])
+console.log(array[2])
+console.log(array[3])
+console.log(array[4])
+
