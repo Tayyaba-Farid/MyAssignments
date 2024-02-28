1689bc38f5318436c8fecf888b57013a6dc076fa
Tayyaba Farid <tayyabafarid7@gmail.com>
2024-02-29 00:36:20 +0500

    Added Assignment 14

---
+++ Assignment 14/file.js
@@ -0,0 +1,5 @@
+// Assignment 14
+var myInvites = ["Sahil Adeem", "Dr.Zakir Naik", "Hitler"];
+myInvites.forEach(function (dinnerinvites) {
+    console.log("Hi, ".concat(dinnerinvites, "! I would like you to join me for dinner."));
+});
---
+++ Assignment 14/file.ts
@@ -0,0 +1,5 @@
+// Assignment 14
+let myInvites = ["Sahil Adeem", "Dr.Zakir Naik", "Hitler"]
+myInvites.forEach(dinnerinvites => {
+    console.log(`Hi, ${dinnerinvites}! I would like you to join me for dinner.`)
+})
