a3f953d57a6d09eb8ddf953bc70b447bc34c64d4
Tayyaba Farid <tayyabafarid7@gmail.com>
2024-03-11 20:32:14 +0500

    Added assignment 19

---
+++ Assignment 19/file.js
@@ -0,0 +1,6 @@
+// Assignment 19
+// taking the program from Assignment 14
+var myInvites = ["Sahil Adeem", "Dr.Zakir Naik", "Hitler"];
+// indicating the number of people invited to dinner
+var numberofpeople = myInvites.length;
+console.log("The number of people invited for dinner are " + "".concat(numberofpeople));
---
+++ Assignment 19/file.ts
@@ -0,0 +1,8 @@
+// Assignment 19
+
+// taking the program from Assignment 14
+let myInvites = ["Sahil Adeem", "Dr.Zakir Naik", "Hitler"]
+
+// indicating the number of people invited to dinner
+let numberofpeople = myInvites.length
+console.log("The number of people invited for dinner are " + `${numberofpeople}`)
