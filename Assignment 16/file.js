6fc9bf309dfc8934ea3ca4bf8e1ad4f542114e7b
Tayyaba Farid <tayyabafarid7@gmail.com>
2024-03-11 20:30:11 +0500

    added Assignment 16

---
+++ Assignment 16/file.js
@@ -0,0 +1,19 @@
+// Assignment 16
+// Part 1: 
+var myInvites = ["Sahil Adeem", "Dr.Zakir Naik", "Hitler"];
+// as Hitler will not be able to attend.
+myInvites[2] = "Hania";
+myInvites.forEach(function (items) {
+    console.log("Hi, " + items + "!" + " " + "I have just got to know that I have found a bigger dinner table!");
+});
+// Part 2: // we will use unshift command to add new element at the start of an array.
+myInvites.unshift("Iqra");
+// part 3: // Adding in the middle
+myInvites.splice(2, 0, "Aqsa");
+console.log(myInvites);
+// adding in the end
+myInvites.push("Umaima");
+// printing new invites 
+myInvites.forEach(function (dinnerinvites) {
+    console.log("Hi, ".concat(dinnerinvites, "! I would like you to join me for dinner."));
+});
