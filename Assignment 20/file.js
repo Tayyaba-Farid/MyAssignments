b668b2021d46d100dd9119304156d012b4a3bdea
Tayyaba Farid <tayyabafarid7@gmail.com>
2024-03-11 20:32:46 +0500

    Added Assignment 20

---
+++ Assignment 20/file.js
@@ -0,0 +1,6 @@
+// Assignment 20
+var countries = ["Pakistan", "Singapore", "USA", "Japan", "Turkey"];
+console.log("list of countries: ");
+countries.forEach(function (mycountry) {
+    console.log(mycountry);
+});
---
+++ Assignment 20/file.ts
@@ -0,0 +1,7 @@
+// Assignment 20
+let countries = ["Pakistan", "Singapore", "USA", "Japan", "Turkey"]
+
+console.log("list of countries: ")
+countries.forEach(mycountry => {
+console.log(mycountry)
+})
