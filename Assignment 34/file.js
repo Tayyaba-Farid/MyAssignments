979e9dc656e489e0d1d0f339fbb2a09ba6a6f876
Tayyaba Farid <tayyabafarid7@gmail.com>
2024-03-12 09:26:30 +0500

    Added Assignment 34

---
+++ Assignment 34/file.js
@@ -0,0 +1,9 @@
+// Assignment 34
+var pizzas = ["pepperoni", "veggie", "fajita"];
+for (var _i = 0, pizzas_1 = pizzas; _i < pizzas_1.length; _i++) {
+    var pizza = pizzas_1[_i];
+    console.log("I like ".concat(pizza, " pizza"));
+}
+console.log("I like pizzas very much.");
+console.log("The taste of pepperoni pizza is very delcious and the veggies are the most fun to eat that's why I like to eat it more than others.");
+console.log("I really love pizza!");
---
+++ Assignment 34/file.ts
@@ -0,0 +1,8 @@
+// Assignment 34
+let pizzas = ["pepperoni", "veggie", "fajita"]
+for(let pizza of pizzas){
+    console.log(`I like ${pizza} pizza`)
+}
+console.log("I like pizzas very much.") 
+console.log("The taste of pepperoni pizza is very delcious and the veggies are the most fun to eat that's why I like to eat it more than others.")
+console.log("I really love pizza!")
