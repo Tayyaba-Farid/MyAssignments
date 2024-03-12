d077daa530944ac613e114cc2e6a1ac34a0cb565
Tayyaba Farid <tayyabafarid7@gmail.com>
2024-03-12 09:08:18 +0500

    Updated Assignment 15

--- Assignment 15/file.js
+++ Assignment 15/file.js
@@ -2,9 +2,10 @@
 var myInvites = ["Sahil Adeem", "Dr.Zakir Naik", "Hitler"];
 // PART 1: 
 var willnotattend = "Hitler";
-console.log("Hitler won't be able to attend as he's not available");
+console.log("As Hitler won't be able to attend because he's not available");
 // Part 2:
 myInvites[2] = "Hania";
+// Part 3:
 myInvites.forEach(function (dinnerinvites) {
     console.log("Hi, ".concat(dinnerinvites, "! I would like you to join me for dinner."));
 });
