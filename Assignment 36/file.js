32b11eaa6407a11321541598474de830313ea649
Tayyaba Farid <tayyabafarid7@gmail.com>
2024-03-12 09:30:04 +0500

    Added Assignment 36

---
+++ Assignment 36/file.js
@@ -0,0 +1,6 @@
+// Assignment 36
+function make_shirt(size, text) {
+    console.log("I have made a size ".concat(size, " shirt with message ").concat(text));
+}
+// calling the function
+make_shirt(20, "I am happy");
---
+++ Assignment 36/file.ts
@@ -0,0 +1,6 @@
+// Assignment 36
+function make_shirt(size: number, text: string){
+console.log(`I have made a size ${size} shirt with message ${text}`)
+}
+// calling the function
+make_shirt(20, "I am happy")
