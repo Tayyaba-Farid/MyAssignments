d6465745c38c4c383b0788b64dddf277e1f5cb2c
Tayyaba Farid <tayyabafarid7@gmail.com>
2024-03-12 09:06:33 +0500

    Added Assignment 23

---
+++ Assignment 23/file.js
@@ -0,0 +1,37 @@
+// Assignment 23
+// Test 1
+var girl = "Tayyaba";
+console.log("Is the girl == 'Tayyaba'? I predict true");
+console.log(girl == "Tayyaba");
+// Test 2
+console.log("Is the girl != 'Ayesha'? I predict true");
+console.log(girl != "Ayesha");
+// Test 3
+console.log("Is girl == 'Tayyaba'? I predict false");
+console.log(girl == "tayyaba");
+// Test 4
+var car = "Honda";
+console.log("Is my bike is 'Honda'? I predict true");
+console.log(car == "Honda");
+// test 5
+console.log("Is my bike is 'Honda'? I predict false");
+console.log(car == "city");
+// test 6
+var num = 10;
+console.log("the number that I have predicted is 10? I predict true");
+console.log(num == 10);
+// test 7
+var bool = false;
+console.log("Is the boolean that I have predicted is false? I predict false");
+console.log(bool == false);
+// test 8
+var boy = "A";
+console.log("Is the first initial of boy is 'A'? I predict false");
+console.log(boy == "T");
+// test 9
+var friend = "Hania";
+console.log("is the friend that I have predicted is 'Hania'? I predict true");
+console.log(friend == "Iqra");
+var id = "tayyaba@h.com";
+console.log("is the id is 'tayyaba@h.com'? I predict false");
+console.log(id == "umaima@h.com");
---
+++ Assignment 23/file.ts
@@ -0,0 +1,46 @@
+// Assignment 23
+// Test 1
+let girl = "Tayyaba";
+console.log("Is the girl == 'Tayyaba'? I predict true")
+console.log(girl == "Tayyaba")
+
+// Test 2
+console.log("Is the girl != 'Ayesha'? I predict true")
+console.log(girl != "Ayesha")
+
+// Test 3
+console.log("Is girl == 'Tayyaba'? I predict false")
+console.log(girl == "tayyaba")
+
+// Test 4
+let car = "Honda"
+console.log("Is my bike is 'Honda'? I predict true")
+console.log(car == "Honda")
+
+// test 5
+console.log("Is my bike is 'Honda'? I predict false")
+console.log(car == "city")
+
+// test 6
+let num = 10;
+console.log("the number that I have predicted is 10? I predict true")
+console.log(num == 10)
+
+// test 7
+let bool = false;
+console.log("Is the boolean that I have predicted is false? I predict false")
+console.log(bool == false)
+
+// test 8
+let boy = "A";
+console.log("Is the first initial of boy is 'A'? I predict false")
+console.log(boy == "T")
+
+// test 9
+let friend = "Hania";
+console.log("is the friend that I have predicted is 'Hania'? I predict true")
+console.log(friend == "Iqra")
+
+let id = "tayyaba@h.com"
+console.log("is the id is 'tayyaba@h.com'? I predict false")
+console.log(id == "umaima@h.com")
