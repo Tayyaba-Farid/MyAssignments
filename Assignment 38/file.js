5695d75ad25299e67cf56c4ff8645634c216f93f
Tayyaba Farid <tayyabafarid7@gmail.com>
2024-03-12 09:31:27 +0500

    Added Assignment 38

---
+++ Assignment 38/file.js
@@ -0,0 +1,8 @@
+// Assignment 38
+function describe_city(city, country) {
+    if (country === void 0) { country = "Turkey"; }
+    console.log("".concat(city, " is in ").concat(country));
+}
+describe_city("Istanbul");
+describe_city("Antalya");
+describe_city("Paris", "France");
---
+++ Assignment 38/file.ts
@@ -0,0 +1,8 @@
+// Assignment 38
+function describe_city(city: string, country: string = "Turkey"){
+console.log(`${city} is in ${country}`)
+} 
+// calling the function
+describe_city("Istanbul")
+describe_city("Antalya")
+describe_city("Paris", "France")
