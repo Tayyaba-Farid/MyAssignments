2f71450d6064762c6e6677ec576e3000fa946c29
Tayyaba Farid <tayyabafarid7@gmail.com>
2024-03-12 09:39:56 +0500

    Added Assignment 39

---
+++ Assignment 39/file.js
@@ -0,0 +1,7 @@
+// Assignment 39
+function city_country(city, country) {
+    console.log("\"".concat(city, ", ").concat(country, "\""));
+}
+city_country("Karachi", "Pakistan");
+city_country("Istanbul", "Turkey");
+city_country("Geneva", "Switzerland");
---
+++ Assignment 39/file.ts
@@ -0,0 +1,7 @@
+// Assignment 39
+function  city_country(city: string, country: string){
+console.log(`"${city}, ${country}"`)
+}
+city_country("Karachi", "Pakistan")
+city_country("Istanbul", "Turkey")
+city_country("Geneva", "Switzerland")
