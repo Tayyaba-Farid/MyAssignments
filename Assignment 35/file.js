e50145d6266d03fc0df9c77a02f2b7c453a3274b
Tayyaba Farid <tayyabafarid7@gmail.com>
2024-03-12 09:27:47 +0500

    Added Assignment 35

---
+++ Assignment 35/file.js
@@ -0,0 +1,7 @@
+// Assignment 35
+var animals = ["Lion", "Tiger", "Leopard"];
+for (var _i = 0, animals_1 = animals; _i < animals_1.length; _i++) {
+    var animal = animals_1[_i];
+    console.log("A ".concat(animal, " would make a great pet"));
+}
+console.log("These animals are such popular animals to choose as a pet and any of them would be a great pet!");
---
+++ Assignment 35/file.ts
@@ -0,0 +1,6 @@
+// Assignment 35
+let animals = ["Lion", "Tiger", "Leopard"]
+for(let animal of animals){
+    console.log(`A ${animal} would make a great pet`)
+}
+console.log("These animals are such popular animals to choose as a pet and any of them would be a great pet!")
