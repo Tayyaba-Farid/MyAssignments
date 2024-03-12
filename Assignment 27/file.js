3243fb4ebddb571f1b12ebd105fcd76bb7b4a58b
Tayyaba Farid <tayyabafarid7@gmail.com>
2024-03-12 09:21:19 +0500

    Added Assignment 27

---
+++ Assignment 27/file.js
@@ -0,0 +1,11 @@
+// Assignment 27
+var alien_colour = "red";
+if (alien_colour == "green") {
+    console.log("Congrats! You have earned 5 points");
+}
+else if (alien_colour == "yellow") {
+    console.log("Congrats! You have earned 10 points");
+}
+else {
+    console.log("Congrats! You have earned 15 points");
+}
---
+++ Assignment 27/file.ts
@@ -0,0 +1,11 @@
+// Assignment 27
+let alien_colour = "yellow";
+if (alien_colour == "green"){
+    console.log("Congrats! You have earned 5 points")
+}
+else if(alien_colour == "yellow"){
+    console.log("Congrats! You have earned 10 points")
+}
+else{
+    console.log("Congrats! You have earned 15 points")
+}
