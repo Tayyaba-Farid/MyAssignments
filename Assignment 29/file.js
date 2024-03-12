7e527c10187806c478054b76c63d599a5bba3547
Tayyaba Farid <tayyabafarid7@gmail.com>
2024-03-12 09:22:34 +0500

    Added Assignment 29

---
+++ Assignment 29/file.js
@@ -0,0 +1,17 @@
+// Assignment 29
+var favorite_fruits = ["strawberries", "mangoes", "watermelon"];
+if (favorite_fruits.includes("apple")) {
+    console.log("apple");
+}
+if (favorite_fruits.includes("strawberries")) {
+    console.log("You really like strawberries");
+}
+if (favorite_fruits.includes("falsey")) {
+    console.log("false");
+}
+if (favorite_fruits.includes("mangoes")) {
+    console.log("You really like mangoes");
+}
+if (favorite_fruits.includes("watermelon")) {
+    console.log("You really like watermelon");
+}
