cf8ab0d70b8e690fbbe2445708491bcd52a8f0ea
Tayyaba Farid <tayyabafarid7@gmail.com>
2024-03-12 08:42:43 +0500

    Added Assignment 21

---
+++ Assignment 21/file.js
@@ -0,0 +1,7 @@
+var newObject = {
+    name: "Tayyaba",
+    email: "tayyaba@8.com",
+    userId: 786,
+    Active: false
+};
+console.log(newObject);
---
+++ Assignment 21/file.ts
@@ -0,0 +1,7 @@
+let newObject: {name: string, email: string, userId: number, Active: boolean} = {
+    name : "Tayyaba",
+    email: "tayyaba@8.com",
+    userId: 786,
+    Active: false
+}
+console.log(newObject)
