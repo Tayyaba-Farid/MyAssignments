5831e193b92c0ca704520e6c8d94836b2ae34790
Tayyaba Farid <tayyabafarid7@gmail.com>
2024-03-14 22:10:31 +0500

    Added Assignment 41

---
+++ Assignment 41/file.js
@@ -0,0 +1,6 @@
+//Assignment 41
+var magicians = ["Wizard Wren", "Magica Marlowe", "Illusory Icarus", "Mystic Miranda"];
+function show_magicians(magicians) {
+    magicians.forEach(function (magicians) { return console.log([magicians]); });
+}
+show_magicians(magicians);
---
+++ Assignment 41/file.ts
@@ -0,0 +1,8 @@
+//Assignment 41
+let magicians: string[] = ["Wizard Wren", "Magica Marlowe", "Illusory Icarus", "Mystic Miranda"]
+function show_magicians(magicians: string[]) {
+    magicians.forEach(magicians => console.log([magicians]))
+}
+  
+
+show_magicians(magicians)
